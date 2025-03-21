00f66945d18e5383bccab2b990d8f88a6123d96a
RashmiV-collab <techteam.dev01@gmail.com>
2025-03-21 15:59:10 +0530

    uploads

---
+++ wp-content/uploads/wpcodebox/2.js
@@ -0,0 +1,9 @@
+// Add your JS Code here, don't include the <script> tags
+
+jQuery(document).ready(function($) {
+
+  $('.single-event_listing .wpem-single-event-ticket-information .wpem-ticket-register-button #order_now').html('Get Tickets');
+
+  $('.page-id-213 .xoo-el-header .xoo-el-login-tgr').html('Back to Login');
+
+})
